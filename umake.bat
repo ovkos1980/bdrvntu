13926d3e5a34bda25a292baeef1760d3027b1e07
Konstantin <ovkos1980@gmail.com>
2022-01-21 20:07:08 +0200

    шаблон для БДР

--- umake.bat
+++ umake.bat
@@ -4,10 +4,10 @@ rem TeXworks->Edit->Preferences->Typesetting->Processing tool->'+'
 rem Name: PdfLaTeX+BibtexU, Program: umake.bat, Arguments: $fullname
 rem or make manually calling umake.bat *.tex from command line
 
-pdflatex %1
-bibtexu %~n1.aux
-pdflatex %1
-pdflatex %1
+pdflatex -shell-escape %1
+bibtex --include-directory="bib/" %~n1.aux
+pdflatex -shell-escape %1
+pdflatex -shell-escape %1
 del *.aux
 del *.bbl
 del *.blg
